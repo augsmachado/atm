88df902de47baf52c440569f6e9af4ca269598c6
Augusto Machado <augustomachadoramos@gmail.com>
2018-08-17 15:41:48 -0300

    Add C++ project for ATM

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("OLA !!\n");
+  system("PAUSE");   
+  return 0;
+}
