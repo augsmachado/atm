8313a25613e760af28216a879a2ce5b5b858aa7a
Augusto Machado <augustomachadoramos@gmail.com>
2018-08-18 12:32:17 -0300

    Add prototype archive of login

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+	Name: ATM system
+	Copyright: MIT License
+	Author: Augusto Machado Ramos
+	Date: 18/08/18 11:35
+	Description: 
+		The goal was to develop an ATM system or a bank self-service platform,
+			from project design,
+			interface design and project architecture to system implementation.
+		In addition to developing the notion of programming in general,
+			know the fundamentals of embedded software programming,
+			in addition to developing advanced knowledge in C / C ++.
+*/
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
